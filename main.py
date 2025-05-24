4e8e7cd3f96c7630b661baca18f674476e5adfa5
Toya <63594019+IamNotMere@users.noreply.github.com>
2025-05-25 00:16:18 +0300

    Update main.py
    -Added a mechanism to support the comparing framework
    -Fixed some errors that occured while running the code
    -Fixed unwanted/mistakenly used functions

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #This will be the main code/ an initiator for the code we will write
 Names = []
 Section = []
+SAS  = []
 Scores = []
+a = ''
+b = ''
 def score_to_grade(grd):
   if grd>=85:
-     print("Grade: A")
+     return("Grade: A")
   elif 85>grd>=70 :
      return("Grade: B")
   elif 70>grd>=50:
@@ -20,23 +23,28 @@ while True:
     sec = str(input("Please the student's section: "))
     s= int(input("Enter a student's grade: ") )
     STG =  score_to_grade(s)
-    Names += n
-    Section += sec
-    Scores += STG
+    Names.append(n)
+    Section.append(sec)
+    Scores.append(STG)
+    for score, sect in zip(Scores, Section):
+       pair = f"{score}:{sect}"
+       if pair not in SAS:
+        SAS.append(pair)
     choice = input('Would you like to sumbit the grade of another student?(Y/N) ')
     if choice.lower() == 'y':
-       break
-    elif choice.lower() == 'n':
        pass
+    elif choice.lower() == 'n':
+       break
     else:
        break    
-  
+
+print(SAS)
 #Here we will have the code to compare the grades of sections
 cs = input("Please input the section's you want to compare.(Eg:- 2,3): ")
+a, b = cs.split(',')
 def compare_section(sec1, sec2):
    if sec1 == sec2:
-      #something happens here
-      pass
+      return('{}')
    elif sec1 >= sec2:
       #another thing also happens here
       pass
